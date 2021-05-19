d3c53b03991ebc8a32fe57ea63a850728be831b8
PatriceManser <59052834+PatriceManser@users.noreply.github.com>
2021-05-19 09:31:59 +0200

    defined the ports and created colour function

--- ampel/ampel.ino
+++ ampel/ampel.ino
@@ -1,21 +1,34 @@
-int Taster=4;
+int Taster=1;
 int tasterstatus=0;
-int ROT=5;
-int GELB=6;
-int GRUN=7;
-int RotFuss=8; // Rotes Ampelsignal für Fußgänger
-int GrunFuss=9; // Grünes Ampelsignal für Fußgänger
+int NORD=0;
+int SUED=3;
+int OST=10;
+int WEST=13;
+int Motor=12;
+int Summer=11;
+int FussgaengerAmpel=7;
 
 void setup() //Wir starten mit dem Setup
-
 {
-pinMode(Taster, INPUT); // Pin 4 ist ein Eingang.
-pinMode(ROT, OUTPUT); // Pin 5 ist ein Ausgang.
-pinMode(GELB, OUTPUT); // Pin 6 ist ein Ausgang.
-pinMode(GRUN,OUTPUT); // Pin 7 ist ein Ausgang.
-pinMode(RotFuss, OUTPUT); // Pin 8 ist ein Ausgang.
-pinMode(GrunFuss,OUTPUT); // Pin 9 ist ein Ausgang.
+pinMode(Taster, INPUT); // Pin 1 ist ein Eingang.
+pinMode(NORD, OUTPUT); // Pin 0 ist ein Ausgang.
+pinMode(SUED, OUTPUT); // Pin 3 ist ein Ausgang.
+pinMode(OST,OUTPUT); // Pin 10 ist ein Ausgang.
+pinMode(WEST,OUTPUT); // Pin 13 ist ein Ausgang.
+pinMode(Motor,OUTPUT); // Pin 12 ist ein Ausgang.
+pinMode(Summer,OUTPUT); // Pin 11 ist ein Ausgang.
+pinMode(FussgaengerAmpel,OUTPUT); // Pin 7 ist ein Ausgang.
 }
+
+
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
+}
+
+
 void loop() // Das Hauptprogramm beginnt.
 {
 digitalWrite(GRUN, HIGH); // Schalte die grüne Ampel an Pin7 an. Die Autos sehen also das grüne Signal
